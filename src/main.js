33e5bc3d56fab00cb131140006b9ded98fe89b96
GerinNugroho <gerinnugroho17@gmail.com>
2026-05-11 17:03:40 +0700

    tailwind css add

--- src/main.js
+++ src/main.js
@@ -1,43 +1,35 @@
-import './style.css';
-import './LoginPage/loginpage.css';
-import './Register/register.css';
-import './Dashboard/dashboard.css';
-import { LoginPage, initLoginLogic } from './LoginPage/loginpage.js';
-import { RegisterPage, initRegisterLogic } from './Register/register.js';
-import { DashboardPage, initDashboardLogic } from './Dashboard/dashboard.js';
+import { LoginPage, initLoginLogic } from "./LoginPage/loginpage.js";
+import { RegisterPage, initRegisterLogic } from "./Register/register.js";
+import { DashboardPage, initDashboardLogic } from "./Dashboard/dashboard.js";
 
-const app = document.querySelector('#app');
+const app = document.querySelector("#app");
 
 function router(page, showBanner = false) {
+  if (page === "dashboard") {
+    document.body.style.display = "block";
+    document.body.style.alignItems = "unset";
+    document.body.style.justifyContent = "unset";
 
-    if (page === 'dashboard') {
-        document.body.style.display = 'block';
-        document.body.style.alignItems = 'unset';
-        document.body.style.justifyContent = 'unset';
+    app.innerHTML = DashboardPage();
+    initDashboardLogic(() => {
+      router("login");
+    });
+  } else {
+    document.body.style.display = "flex";
+    document.body.style.alignItems = "center";
+    document.body.style.justifyContent = "center";
 
-        app.innerHTML = DashboardPage();
-        initDashboardLogic(() => {
-            router('login');
-        });
-
-    } else {
-      
-        document.body.style.display = 'flex';
-        document.body.style.alignItems = 'center';
-        document.body.style.justifyContent = 'center';
-
-        if (page === 'login') {
-            app.innerHTML = LoginPage(showBanner);
-            initLoginLogic(
-                () => router('dashboard'),  
-                () => router('register')    
-            );
-
-        } else if (page === 'register') {
-            app.innerHTML = RegisterPage();
-            initRegisterLogic((fromRegister) => router('login', fromRegister));
-        }
+    if (page === "login") {
+      app.innerHTML = LoginPage(showBanner);
+      initLoginLogic(
+        () => router("dashboard"),
+        () => router("register"),
+      );
+    } else if (page === "register") {
+      app.innerHTML = RegisterPage();
+      initRegisterLogic((fromRegister) => router("login", fromRegister));
     }
+  }
 }
 
-router('login');
+router("login");
