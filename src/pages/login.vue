4aea1de469e95e8ad7a62954ebdf0f1c5eb8bd08
GerinNugroho <gerinnugroho17@gmail.com>
2026-05-17 12:37:58 +0700

    remember me added

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -23,6 +23,12 @@
                 <fieldInput ref="inputPassword" name="password" label="password" type="password"
                     placeholder="●●●●●●●●" />
 
+                <label class="checkContainer">
+                    <input type="checkbox" class="opacity-0 w-0 h-0  absolute cursor-pointer">
+                    <span class="checkMark"></span>
+                    Remember Me
+                </label>
+
                 <button type="submit" class="btn-login">
                     <span>Sign In</span>
                     <span></span>
@@ -30,7 +36,7 @@
             </form>
             <p class="text-secondary text-center mt-3 font-mono">
                 Don't have an account?
-                <a href="" class="text-accent">
+                <a href="#" class="text-accent">
                     Sign Up
                 </a>
             </p>
@@ -58,4 +64,42 @@ function focusNextInput(nextComponent) {
 
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+@import "../style.css";
+
+.checkContainer {
+    @apply text-muted font-mono relative cursor-pointer select-none pl-6;
+}
+
+.checkMark {
+    @apply absolute top-[50%] translate-y-[-50%] left-0 h-5 w-5 bg-input transition-[smooth] rounded-sm;
+}
+
+.checkContainer:hover input~.checkMark {
+    @apply border-accent border-solid border;
+}
+
+.checkContainer input:checked~.checkMark {
+    @apply bg-accent;
+}
+
+/* .checkMark::after {
+    content: '';
+    @apply absolute left-0 top-0 w-full h-full;
+    display: none;
+    background: url('../assets/check.svg');
+    background-size: cover;
+    background-position: center;
+} */
+
+.checkContainer input:checked~.checkMark::after {
+    content: '';
+    @apply absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%];
+    display: block;
+    width: 9px;
+    height: 5px;
+    border-left: 2px solid #fff;
+    border-bottom: 2px solid #fff;
+    transform: rotate(-45deg) translateY(-1px);
+}
+</style>
